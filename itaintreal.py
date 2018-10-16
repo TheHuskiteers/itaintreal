1db68f6d59ae12279213930824e2d117ae27fa9e
Jacob Dillon <jepityr@sdf.org>
2018-10-16 01:19:40 -0400

    Initial commit

---
+++ itaintreal.py
@@ -0,0 +1,23 @@
+import random
+
+from flask import (Flask, render_template)
+from pathlib import Path
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    (subject, actor, purpose) = get_data()
+    return render_template('index.html', subject=subject, actor=actor, purpose=purpose)
+    
+def get_data():
+    subject = get_random_line_from_file(Path('data/subject.txt'))
+    actor = get_random_line_from_file(Path('data/actor.txt'))
+    purpose = get_random_line_from_file(Path('data/purpose.txt'))
+
+    return (subject, actor, purpose)
+
+def get_random_line_from_file(path):
+    lines = open(path, 'r').read().splitlines()
+    return random.choice(lines)
